59d4b5bb44f677a6fa25060899647b9867983953
Shawn Lu <lu.shangchiun@gmail.com>
2022-08-31 11:01:22 +0800

    Updated to TCP connection
    Signed-off-by: Shawn Lu <lu.shangchiun@gmail.com>

--- lib/screen/screen_connect.dart
+++ lib/screen/screen_connect.dart
@@ -1,5 +1,4 @@
 import 'dart:convert';
-import 'dart:developer';
 import 'dart:io';
 import 'dart:typed_data';
 
@@ -22,6 +21,7 @@ class _ConnectionScreenState extends State<ConnectionScreen> {
   final TextEditingController ssidController = TextEditingController(text: 'HiAp');
   final TextEditingController pwController = TextEditingController(text: 'BB9ESERVER');
   String statusDescription = "未連線";
+  ConnectState connectState = ConnectState.idle;
 
   @override
   Widget build(BuildContext context) {
@@ -30,55 +30,46 @@ class _ConnectionScreenState extends State<ConnectionScreen> {
         stream: state.connectStateStream,
         initialData: ConnectState.idle,
         builder: (context, snapshot) {
+          connectState = snapshot.data ?? ConnectState.idle;
           statusDescription = getStatusDescription(snapshot.data ?? ConnectState.idle);
           return Scaffold(
             appBar: AppBar(
-              title: Text('Connect to UDP server'),
+              title: Text('連接'),
             ),
             body: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Center(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      '1. 連接到 Access Point',
-                      style: TextStyle(fontWeight: FontWeight.bold),
-                    ),
-                    TextField(
-                      controller: ssidController,
-                      decoration: InputDecoration(hintText: 'AP SSID'),
-                    ),
-                    TextField(
-                      controller: pwController,
-                      decoration: InputDecoration(hintText: 'AP Password'),
-                    ),
-                    ElevatedButton(
-                        onPressed: () {
-                          connect();
-                        },
-                        child: Text('開始連接')),
-                    Text(statusDescription),
-                    Padding(
-                      padding: const EdgeInsets.only(top: 32.0),
-                      child: Text('2. 傳送 Hello 訊息', style: TextStyle(fontWeight: FontWeight.bold)),
-                    ),
-                    ElevatedButton(
-                        onPressed: () {
-                          udpManager.write(const Utf8Codec().encode('Hello from client'));
-                        },
-                        child: Text('hello')),
-                    Spacer(),
-                    MaterialButton(
-                        onPressed: () {
-                          Navigator.of(context).push(MaterialPageRoute(builder: (c) => HomeScreen()));
-                        },
+              padding: const EdgeInsets.all(24.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    '連接到裝置',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  TextField(
+                    controller: ssidController,
+                    decoration: InputDecoration(hintText: 'AP SSID'),
+                  ),
+                  TextField(
+                    controller: pwController,
+                    decoration: InputDecoration(hintText: 'AP Password'),
+                  ),
+                  SizedBox(width: double.infinity, child: getAction(connectState)),
+                  Text(statusDescription),
+                  const Spacer(),
+                  SizedBox(
+                    height: 48,
+                    child: ElevatedButton(
+                        onPressed: connectState == ConnectState.tcpconnected
+                            ? () {
+                                Navigator.of(context).push(MaterialPageRoute(builder: (c) => HomeScreen()));
+                              }
+                            : null,
                         child: Row(
                           mainAxisAlignment: MainAxisAlignment.center,
                           children: [Icon(Icons.home), Text('GO')],
-                        ))
-                  ],
-                ),
+                        )),
+                  )
+                ],
               ),
             ),
           );
@@ -86,6 +77,7 @@ class _ConnectionScreenState extends State<ConnectionScreen> {
   }
 
   void connect() async {
+    state.setState(ConnectState.wificonnecting);
     await WiFiForIoTPlugin.connect(ssidController.text,
         password: pwController.text, joinOnce: true, security: NetworkSecurity.WPA);
     udpManager.isConnected = await WiFiForIoTPlugin.isConnected();
@@ -99,15 +91,19 @@ class _ConnectionScreenState extends State<ConnectionScreen> {
     }
   }
 
+  void disconnect() {
+    udpManager.close();
+    WiFiForIoTPlugin.disconnect();
+  }
+
   void bindTcp() async {
-    
     var addressesIListenFrom = InternetAddress.anyIPv4;
     int portIListenOn = 1234; //0 is random
     state.setState(ConnectState.tcpconnecting);
     try {
       Socket socket = await Socket.connect('192.168.4.1', portIListenOn);
       socket.listen(
-            (Uint8List event) {
+        (Uint8List event) {
           if (state.connectState != ConnectState.tcpconnected) {
             state.setState(ConnectState.tcpconnected);
           }
@@ -126,7 +122,7 @@ class _ConnectionScreenState extends State<ConnectionScreen> {
       // send hello
       udpManager.socket = socket;
       udpManager.write(utf8.encode('hello'));
-    }catch(e){
+    } catch (e) {
       state.setState(ConnectState.idle);
     }
   }
@@ -143,6 +139,29 @@ class _ConnectionScreenState extends State<ConnectionScreen> {
         return '正在建立TCP連線';
       case ConnectState.tcpconnected:
         return '連線成功';
+    }
+  }
+
+  Widget getAction(ConnectState state) {
+    switch (state) {
+      case ConnectState.idle:
+        return ElevatedButton(
+            onPressed: () {
+              connect();
+            },
+            child: Text('開始連接'));
+      case ConnectState.tcpconnecting:
+      case ConnectState.wificonnecting:
+        return ElevatedButton(onPressed: null, child: Text('...'));
+      default:
+        return ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              primary: Colors.red,
+            ),
+            onPressed: () {
+              disconnect();
+            },
+            child: Text('中斷連線'));
     }
   }
 }
